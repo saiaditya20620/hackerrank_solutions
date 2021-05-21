c18dd90a91741b199192ddf13a2d420f84f46129
saiaditya20620 <82864402+saiaditya20620@users.noreply.github.com>
2021-05-21 17:00:49 +0530

    Update gradingstudents.py

--- gradingstudents.py
+++ gradingstudents.py
@@ -1,1 +1,12 @@
-
+k=[]
+for i in l:
+    if i<=35:
+        k.append(i)
+    if i>35:
+        x=i%5
+        if x>=3:
+            i=i+abs(x-5)
+            k.append(i)
+        else:
+            k.append(i)
+return k
